dc3344f6d26a50afdc9172ef722dcd9fe1156dbb
zdogar556 <zaindogar556@gmail.com>
2025-08-13 22:09:51 +0500

    Remove background color of charts in feed

--- client/src/pages/feed/Feed.jsx
+++ client/src/pages/feed/Feed.jsx
@@ -110,12 +110,12 @@ const Feed = () => {
   }, []);
 
   const feedColors = {
-    Starter: "bg-blue-100 text-blue-800 border-blue-400",
-    Grower: "bg-green-100 text-green-800 border-green-400",
-    Finisher: "bg-yellow-100 text-yellow-800 border-yellow-400",
-    Layer: "bg-purple-100 text-purple-800 border-purple-400",
-    Broiler: "bg-pink-100 text-pink-800 border-pink-400",
-    Medicated: "bg-red-100 text-red-800 border-red-400",
+    Starter: " text-blue-800 border-blue-400",
+    Grower: " text-green-800 border-green-400",
+    Finisher: " text-yellow-800 border-yellow-400",
+    Layer: " text-purple-800 border-purple-400",
+    Broiler: " text-pink-800 border-pink-400",
+    Medicated: " text-red-800 border-red-400",
   };
 
   const modalVariants = {
