189e44371817f7d5483f975abc722ea88d98db03
zdogar556 <zaindogar556@gmail.com>
2025-08-20 12:56:14 +0500

    Add Label in forgot password

--- client/src/pages/auth/Forgotpassword.jsx
+++ client/src/pages/auth/Forgotpassword.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { HiMail, HiLockClosed, HiEye, HiEyeOff } from "react-icons/hi";
 
 const ForgotPassword = () => {
@@ -39,10 +40,13 @@ const ForgotPassword = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-6 text-center">Reset Password</h2>
+        <h2 className="text-2xl font-bold mb-6 text-center">Forgot Password</h2>
         <form onSubmit={handleSubmit}>
 
           {/* Email with Icon */}
+           <label className="block font-medium text-gray-600 mb-2">
+              Email
+            </label>
           <div className="relative mb-4">
             <span className="absolute left-3 top-3 text-gray-500">
               <HiMail size={20} />
@@ -58,6 +62,9 @@ const ForgotPassword = () => {
           </div>
 
           {/* New Password with Icon + Eye Toggle */}
+          <label className="block font-medium text-gray-600 mb-2">
+              Enter new password
+            </label>
           <div className="relative mb-4">
             <span className="absolute left-3 top-3 text-gray-500">
               <HiLockClosed size={20} />
@@ -79,6 +86,9 @@ const ForgotPassword = () => {
           </div>
 
           {/* Confirm Password with Icon + Eye Toggle */}
+          <label className="block font-medium text-gray-600 mb-2">
+              Confrim new password
+            </label>
           <div className="relative mb-6">
             <span className="absolute left-3 top-3 text-gray-500">
               <HiLockClosed size={20} />
@@ -101,11 +111,17 @@ const ForgotPassword = () => {
 
           <button
             type="submit"
-            className="w-full bg-blue-500 text-white py-3 rounded hover:bg-blue-600"
+            className="w-full rounded-lg bg-blue-600 px-4 py-2 text-white transition hover:bg-blue-700"
           >
-            Reset Password
+            Submit
           </button>
         </form>
+        <p className="mt-4 text-center text-sm text-gray-500">
+          Got it?{" "}
+          <Link to="/" className="text-blue-600">
+            Go Back
+          </Link>
+        </p>
       </div>
     </div>
   );
